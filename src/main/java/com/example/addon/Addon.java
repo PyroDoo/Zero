eaeb23e5738bb613a18ae61ead6c82f36ef3ec4b
PyroDoo <102172432+PyroDoo@users.noreply.github.com>
2023-02-05 23:17:29 +0530

    Update Addon.java

--- src/main/java/com/example/addon/Addon.java
+++ src/main/java/com/example/addon/Addon.java
@@ -15,7 +15,7 @@ import org.slf4j.Logger;
 public class Addon extends MeteorAddon {
     public static final Logger LOG = LogUtils.getLogger();
     public static final Category CATEGORY = new Category("Example");
-    public static final HudGroup HUD_GROUP = new HudGroup("Example"); Hud.get().register(LogoHud.INFO);
+    public static final HudGroup HUD_GROUP = new HudGroup("Example");
 
     @Override
     public void onInitialize() {
