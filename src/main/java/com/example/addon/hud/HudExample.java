2ecf47c7508db19b089a03219e334548af766718
PyroDoo <102172432+PyroDoo@users.noreply.github.com>
2023-02-05 22:24:59 +0530

    Update HudExample.java

--- src/main/java/com/example/addon/hud/HudExample.java
+++ src/main/java/com/example/addon/hud/HudExample.java
@@ -1,22 +1,59 @@
 package com.example.addon.hud;
 
-import com.example.addon.Addon;
+import meteordevelopment.meteorclient.utils.render.color.Color;
+import com.example.addon.hud;
+import meteordevelopment.meteorclient.settings.BoolSetting;
+import meteordevelopment.meteorclient.settings.DoubleSetting;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.hud.HudElement;
 import meteordevelopment.meteorclient.systems.hud.HudElementInfo;
 import meteordevelopment.meteorclient.systems.hud.HudRenderer;
-import meteordevelopment.meteorclient.utils.render.color.Color;
+import net.minecraft.util.Identifier;
 
 public class HudExample extends HudElement {
-    public static final HudElementInfo<HudExample> INFO = new HudElementInfo<>(Addon.HUD_GROUP, "example", "HUD element example.", HudExample::new);
 
-    public HudExample() {
-        super(INFO);
-    }
+	public static final HudElementInfo<HudExample> INFO = new HudElementInfo<>(Addon.HUD_GROUP, "MeteorPlusLogo", "Shows the MeteorPlus logo in the HUD.", HudExample::new);
+	private final SettingGroup sgGeneral = settings.getDefaultGroup();
 
-    @Override
-    public void render(HudRenderer renderer) {
-        setSize(renderer.textWidth("Example element", true), renderer.textHeight(true));
+	private final Setting<Double> scale = sgGeneral.add(new DoubleSetting.Builder()
+		.name("scale")
+		.description("The scale of the logo.")
+		.defaultValue(3)
+		.min(0.1)
+		.onChanged((size) -> calculateSize())
+		.sliderRange(0.1, 10)
+		.build()
+	);
 
-        renderer.text("Example element", x, y, Color.WHITE, true);
-    }
+	private final Setting<Boolean> invert = sgGeneral.add(new BoolSetting.Builder()
+		.name("invert")
+		.description("Invert the logo.")
+		.defaultValue(false)
+		.build()
+	);
+
+	private final Identifier TEXTURE = new Identifier("plus", "logo.png");
+
+
+	public HudExample() {
+		super(INFO);
+		calculateSize();
+	}
+
+	public void calculateSize() {
+		box.setSize(64 * scale.get(), 50 * scale.get());
+	}
+
+	@Override
+	public void render(HudRenderer renderer) {
+		int w = getWidth();
+		int h = getHeight();
+
+		if (!invert.get()) {
+			renderer.texture(TEXTURE, box.getRenderX(), box.getRenderY(), w, h, Color.WHITE);
+		} else {
+			renderer.texture(TEXTURE, box.getRenderX()+w, box.getRenderY(), -w, h, Color.WHITE);
+		}
+	}
 }
